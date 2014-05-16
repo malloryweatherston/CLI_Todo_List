4104adb8f18bfb6a739fd1fd6af5987b0985559c
malloryweatherston <mjw0909@sbcglobal.net>
2014-05-16 12:01:15 -0500

    Added function to trim, and convert to upper

--- todo.php
+++ todo.php
@@ -1,4 +1,23 @@
 <?php
+// List array items formatted for CLI
+function list_items($list)
+{
+    // Return string of list items separated by newlines.
+    // Should be listed [KEY] Value like this:
+    // [1] TODO item 1
+    // [2] TODO item 2 - blah
+    // DO NOT USE ECHO, USE RETURN
+    return 
+}
+
+// Get STDIN, strip whitespace and newlines, 
+// and convert to uppercase if $upper is true
+function get_input($upper = FALSE) { 
+    // Return filtered STDIN input
+    if (get_input($upper = TRUE)) {
+        return trim(strtoupper($upper));
+    }
+} 
 
 // Create array to hold list of todo items
 $items = array();
@@ -10,8 +29,8 @@ do {
     // Iterate through list items
     foreach ($items as $key => $item) {
         // Display each item and a newline
-        $key1 = $key + 1; 
-        echo "[{$key1}] {$item}\n";
+        $key1 = $key++; 
+        echo "[{$key}] {$item}\n";
     }
 
     // Show the menu options
@@ -19,21 +38,25 @@ do {
 
     // Get the input from user
     // Use trim() to remove whitespace and newlines
-    $input = trim(strtoupper(fgets(STDIN)));
+    $input = get_input(TRUE);
 
     // Check for actionable input
     if ($input == "N") {
         // Ask for entry
         echo 'Enter item: ';
         // Add entry to list array
-        $items[] = trim(fgets(STDIN));
+        $items[] = get_input();
     } elseif ($input == "R") {
-        // Remove which item? 
+        // Remove which item?  
         echo 'Enter item number to remove: ';
         // Get array key
-        $key = trim(fgets(STDIN));
+        $key = get_input();
         // Remove from array
+        $key--; 
         unset($items[$key]);
+        // $items = array_values($items);
+        // array_unshift($items, "");
+        // unset($items[0]); 
     }
 // Exit when input is (Q)uit
 } while ($input != "Q");
